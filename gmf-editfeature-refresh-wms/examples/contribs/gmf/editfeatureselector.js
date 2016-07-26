447ba0ec99eacd5e31c4c4564ff9550c960ed063
Alexandre Dubé <adube@mapgears.com>
2016-07-26 08:10:32 -0400

    Update GitHub pages

--- gmf-editfeature-refresh-wms/examples/contribs/gmf/editfeatureselector.js
+++ gmf-editfeature-refresh-wms/examples/contribs/gmf/editfeatureselector.js
@@ -26,13 +26,14 @@ app.module.constant('gmfLayersUrl',
  * @param {!angular.Scope} $scope Angular scope.
  * @param {gmf.Themes} gmfThemes The gmf themes service.
  * @param {gmfx.User} gmfUser User.
+ * @param {ngeo.FeatureHelper} ngeoFeatureHelper Ngeo feature helper service.
  * @param {ngeo.LayerHelper} ngeoLayerHelper Ngeo Layer Helper.
  * @param {ngeo.ToolActivateMgr} ngeoToolActivateMgr Ngeo ToolActivate manager
  *     service.
  * @constructor
  */
-app.MainController = function($scope, gmfThemes, gmfUser, ngeoLayerHelper,
-    ngeoToolActivateMgr) {
+app.MainController = function($scope, gmfThemes, gmfUser, ngeoFeatureHelper,
+    ngeoLayerHelper, ngeoToolActivateMgr) {
 
   /**
    * @type {!angular.Scope}
@@ -45,6 +46,12 @@ app.MainController = function($scope, gmfThemes, gmfUser, ngeoLayerHelper,
    * @export
    */
   this.gmfUser = gmfUser;
+
+  /**
+   * @type {ngeo.FeatureHelper}
+   * @private
+   */
+  this.featureHelper_ = ngeoFeatureHelper;
 
   gmfThemes.loadThemes();
 
@@ -71,7 +78,68 @@ app.MainController = function($scope, gmfThemes, gmfUser, ngeoLayerHelper,
     source: new ol.source.Vector({
       wrapX: false,
       features: new ol.Collection()
-    })
+    }),
+    style: function(feature, resolution) {
+      // (1) Style definition depends on geometry type
+      var white = [255, 255, 255, 1];
+      var blue = [0, 153, 255, 1];
+      var width = 3;
+      var styles = [];
+
+      var geom = feature.getGeometry();
+      console.assert(geom);
+      var type = geom.getType();
+
+      if (type === 'Point') {
+        styles.push(
+          new ol.style.Style({
+            image: new ol.style.Circle({
+              radius: width * 2,
+              fill: new ol.style.Fill({
+                color: blue
+              }),
+              stroke: new ol.style.Stroke({
+                color: white,
+                width: width / 2
+              })
+            }),
+            zIndex: Infinity
+          })
+        );
+      } else {
+        if (type === 'LineString') {
+          styles.push(
+            new ol.style.Style({
+              stroke: new ol.style.Stroke({
+                color: white,
+                width: width + 2
+              })
+            })
+          );
+          styles.push(
+            new ol.style.Style({
+              stroke: new ol.style.Stroke({
+                color: blue,
+                width: width
+              })
+            })
+          );
+        } else {
+          styles.push(
+            new ol.style.Style({
+              fill: new ol.style.Fill({
+                color: [255, 255, 255, 0.5]
+              })
+            })
+          );
+        }
+
+        // (2) Anything else than 'Point' requires the vertex style as well
+        styles.push(ngeoFeatureHelper.getVertexStyle(true));
+      }
+
+      return styles;
+    }.bind(this)
   });
 
   /**
